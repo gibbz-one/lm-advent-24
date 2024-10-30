43014d5eb4a668628a1917f1d7b6f6576548377d
Daniele Gibertoni <daniele@zuccaezafferano.it>
2024-10-30 16:45:03 +0100

    files

---
+++ adv24.js
@@ -0,0 +1,349 @@
+var adv24 = {
+  initData: [
+    {
+      id: 1,
+      target_url: 'https://www.leroymerlin.it/offerte/vendite-flash-illuminazione/',
+      placeholder: "brown.png",
+      valid_until: '10/12/2024'
+    }, //1
+    {
+      id: 2,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-2/',
+      placeholder: "green.png",
+      valid_until: '31/12/2024'
+    },
+    {
+      id: 3,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-3/',
+      placeholder: "red.png",
+      valid_until: '03/12/2024'
+    },
+    {
+      id: 4,
+      target_url: 'https://www.leroymerlin.it/prodotti/natale/alberi-di-natale/alberi-di-natale/alberi-di-natale-210-cm/albero-di-natale-artificiale-marmolada-verde-h-210-cm-x-135-cm-88662740.html',
+      placeholder: "brown.png",
+      valid_until: '23/12/2024'
+    },
+    {
+      id: 5,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-5/',
+      placeholder: "green.png",
+      valid_until: '05/12/2024'
+    },
+    {
+      id: 6,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-6/',
+      placeholder: "red.png",
+      valid_until: '13/12/2024'
+    },
+    {
+      id: 7,
+      target_url: 'https://www.leroymerlin.it/prodotti/utensileria/elettroutensili/trapani-e-avvitatori/trapani-avvitatori-a-batteria/trapano-avvitatore-a-batteria-ryobi-r18ck2-2c20g-18-v-2-ah-2-batterie-92216366.html',
+      placeholder: "brown.png",
+      valid_until: '23/12/2024'
+    },
+    {
+      id: 8,
+      target_url: 'https://www.leroymerlin.it/prodotti/ordine-e-sistemazione/armadi-e-bauli/armadi-portascope/armadio-per-scope-qbik-basic-recycled-garofalo-in-pvc-grigio-antracite-l-67-x-h-169-x-p-35-cm-2-ante-adatto-per-interno-esterno-92421471.html',
+      placeholder: "green.png",
+      valid_until: '23/12/2024'
+    },
+    {
+      id: 9,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-9/',
+      placeholder: "green.png",
+      valid_until: '13/12/2024'
+    },
+    {
+      id: 10,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-10/',
+      placeholder: "green.png",
+      valid_until: '19/12/2024'
+    }, //10
+    {
+      id: 11,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-11/',
+      placeholder: "green.png",
+      valid_until: '17/12/2024'
+    },
+    {
+      id: 12,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-12/',
+      placeholder: "green.png",
+      valid_until: '12/12/2024'
+    },
+    {
+      id: 13,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-13/',
+      placeholder: "brown.png",
+      valid_until: '19/12/2024'
+    },
+    {
+      id: 14,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-14/',
+      placeholder: "red.png",
+      valid_until: '29/12/2024'
+    },
+    {
+      id: 15,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-15/',
+      placeholder: "brown.png",
+      valid_until: '30/12/2024'
+    },
+    {
+      id: 16,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-16/',
+      placeholder: "red.png",
+      valid_until: '22/12/2024'
+    },
+    {
+      id: 17,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-17/',
+      placeholder: "red.png",
+      valid_until: '11/01/2025'
+    },
+    {
+      id: 18,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-18/',
+      placeholder: "green.png",
+      valid_until: '24/12/2024'
+    },
+    {
+      id: 19,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-19/',
+      placeholder: "red.png",
+      valid_until: '09/01/2025'
+    },
+    {
+      id: 20,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-20/',
+      placeholder: "green.png",
+      valid_until: '01/02/2025'
+    }, // 20
+    {
+      id: 21,
+      target_url: 'https://www.leroymerlin.it/prodotti/utensileria/bidoni-aspiratutto/bidoni-aspiratutto-e-aspiratori/aspiratore-solidi-e-liquidi-bosch-advancedvac-20-aspirazione-26-kpa-20-l-1200-w-36290016.html',
+      placeholder: "brown.png",
+      valid_until: '23/12/2024'
+    },
+    {
+      id: 22,
+      target_url: 'https://www.leroymerlin.it/prodotti/ordine-e-sistemazione/armadi-e-bauli/armadi-da-esterno/armadio-basso-qbik-basic-recycled-garofalo-in-pvc-grigio-antracite-l-67-x-h-84-x-p-35-cm-2-ante-adatto-per-interno-esterno-92421467.html',
+      placeholder: "green.png",
+      valid_until: '23/12/2024'
+    },
+    {
+      id: 23,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-23/',
+      placeholder: "red.png",
+      valid_until: '03/01/2025'
+    },
+    {
+      id: 24,
+      target_url: 'https://www.leroymerlin.it/offerte/calendario-dell-avvento-giorno-24/',
+      placeholder: "red.png",
+      valid_until: '30/12/2024'
+    }, // 24
+  ],
+
+  imagesBackPath: 'images/backs/',
+
+  headerStyleElem: null,
+
+  currentDay: null,
+
+  currentDate: null,
+
+  isAfterEve: false,
+
+  init: function () {
+    var _this = this;
+
+    this.setupImages();
+
+    this.copyNodes();
+
+    setTimeout(function(){ 
+      let el = document.querySelector('.adv-24-back-item.adv-24-vis-hidden');
+      if(el){
+        el.classList.remove('adv-24-vis-hidden')
+      }
+      _this.startAnimation();
+    }, 1800);
+  },
+  init_old: function () {
+
+
+    $(window).scroll(function () {
+      var scrollLocation = $(document).scrollTop();
+      var vanishingPoint = scrollLocation + window.innerHeight / 2;
+      $(".adv24-window-anim").css('perspective-origin', ' 50% ' + vanishingPoint + 'px');
+      var a = $(".adv24-window-anim")[0]
+
+      console.log(a.offsetHeight);
+      $(".adv24-window-anim").css('transform-origin', 'top left');
+    })
+
+  },
+  setupImages: function () {
+    let base_path = "images/days/";
+    let placholder_path = "images/placeholders/";
+    const nodes = document.querySelectorAll('.adv24-wrapper img.adv24-day-image');
+
+    this.currentDate = new Date();
+
+    var today = new Date().getDate();
+
+    const minDate = new Date(2024, 11, 1, 0, 0, 0); // 1 dicembre 2024
+
+    const maxDate = new Date(2024, 11, 24, 23, 59, 59); // 24 dicembre 2024
+   
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const dayParam = urlParams.get('day')
+    if (dayParam) today = dayParam;
+
+    const regex = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/;
+
+    const dateParam = urlParams.get('date')
+    if (dateParam && regex.test(dateParam)) {
+      try {
+        this.currentDate = this.convertToDate(dateParam);
+        if (this.currentDate >= minDate && this.currentDate <= maxDate ) {
+          today = this.currentDate.getDate()
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    if (this.currentDate < minDate) {
+      today = 1;
+    }
+    
+    if (this.currentDate > maxDate) {
+      today = 24;
+      this.isAfterEve = true;
+      document.querySelector('.adv24-container').classList.add('adv24-after-eve');
+    }
+
+
+    console.log("🚀 ~ today:", today)
+
+    if(today < 1) today = 1; 
+    if(today > 24) today = 24; 
+
+    this.currentDay = today;
+
+    const placeholder = this.initData[today - 1].placeholder
+    const panelColor = placeholder.includes('green') ? '#01572e' : (placeholder.includes('red') ? '#7b000f' : '#a06908');
+
+    if (!this.headerStyleElem) {
+      this.headerStyleElem = document.head.appendChild(document.createElement("style"));
+    }
+    this.headerStyleElem.innerHTML = ".adv24-panel-inner:after {background: " + panelColor + ";}";
+
+    const panels = document.querySelectorAll('.adv24-panel-inner');
+    panels.forEach(box => {
+      box.style.backgroundImage = "url(" + base_path + today + ".png)"
+    })
+
+    this.setupMainBack(today);
+
+    for (var i = 1; i <= 24; i++) {
+      if (i == today) {
+        nodes[i - 1].parentNode.classList.add('adv24-day-box-today');
+        nodes[i - 1].src = placholder_path + this.initData[i - 1].placeholder;
+      } else {
+        if (i < today) {
+          this.setDayBack(i);
+        } else {
+          nodes[i - 1].src = base_path + i + ".png";
+        }
+
+      }
+    }
+
+    if(this.isAfterEve){
+      document.querySelector('.adv24-house-top img').src= 'images/casetta_mobile_top_after_eve.png'
+      this.setDayBack(24);
+    }
+  },
+  copyNodes: function () {
+    const elementToCopy = document.querySelector('.adv24-wrapper');
+
+    const childElements = elementToCopy.children;
+
+    const clonedElement = elementToCopy.cloneNode(true); // 'true' means deep clone (with child nodes)
+
+    const targetDiv = document.querySelector('.adv24-house-days');
+
+    Array.from(childElements).forEach((child) => {
+      const clonedChild = child.cloneNode(true);
+      targetDiv.appendChild(clonedChild);
+    });
+
+  },
+
+  setupMainBack: function (day) {
+    const templates = document.querySelectorAll('.adv-24-templates .adv-24-back-item');
+    const template = templates[1];
+    const targetDiv = document.querySelector('.adv24-window-anim');
+    const cloned = template.cloneNode(true);
+
+    const url = this.initData[day - 1].target_url;
+    cloned.classList.add('adv-24-vis-hidden');
+    cloned.querySelector('.adv-24-back-item-link').href = url;
+    cloned.querySelector('.adv-24-back-item-link-full').href = url;
+    cloned.querySelector('.adv-24-back-item-img').src = this.imagesBackPath + day + "_open.png";
+
+    targetDiv.appendChild(cloned);
+  },
+
+  setDayBack: function (day) {
+    const templates = document.querySelectorAll('.adv-24-templates .adv-24-back-item');
+    const template = templates[1];
+    const boxes = document.querySelectorAll('.adv24-wrapper .adv24-day-box');
+    const targetDiv = boxes[day - 1];
+    targetDiv.innerHTML = '';
+    const cloned = template.cloneNode(true);
+
+    const url = this.initData[day - 1].target_url;
+    cloned.querySelector('.adv-24-back-item-link').href = url;
+    cloned.querySelector('.adv-24-back-item-link-full').href = url;
+    cloned.querySelector('.adv-24-back-item-img').src = this.imagesBackPath + day + "_open.png";
+    cloned.classList.add('adv-24-day-grow');
+
+    if(!this.isPromotionValid(day)){
+      cloned.classList.add('adv24-promo-disabled')
+    }
+
+    targetDiv.appendChild(cloned);
+  },
+
+  startAnimation: function () {
+    const panelLeft = document.querySelector('.adv24-window-left-panel');
+    panelLeft.style.animationName = 'panel-left';
+
+    const panelRight = document.querySelector('.adv24-window-right-panel');
+    panelRight.style.animationName = 'panel-right';
+  },
+
+  convertToDate: function(dateString){
+    const [day, month, year] = dateString.split('/').map(Number);
+    return new Date(year, month - 1, day);
+  },
+
+  isPromotionValid:function(day){
+    let until_date = this.convertToDate(this.initData[day-1].valid_until);
+    until_date.setHours(23);
+    until_date.setMinutes(59);
+    until_date.setSeconds(59);
+    //const now_date = new Date(2024,11,this.currentDay);
+    return this.currentDate < until_date;
+  }
+};
+
+(function () {
+  adv24.init();
+})();
